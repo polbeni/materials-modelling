351f2635be958e68416b4c57507ab7215766bb14
Pol <polbeco@gmail.com>
2024-10-14 17:31:26 +0100

    improved version to plot the bands where the distance between high symmetry points in the reciprocal space is considered

--- python-code/energy-band-plot/get-bands.py
+++ python-code/energy-band-plot/get-bands.py
@@ -1,111 +1,298 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
+# Pol Benítez Colominas, September-October 2024
+# University of Cambridge and Universitat Politècnica de Catalunya
 
-kpoint1 = np.zeros(192)
-kpoint2 = np.zeros(192)
-kpoint3 = np.zeros(192)
-kpoint4 = np.zeros(192)
-kpoint5 = np.zeros(192)
-kpoint6 = np.zeros(192)
-kpoint7 = np.zeros(192)
-kpoint8 = np.zeros(192)
-kpoint9 = np.zeros(192)
-kpoint10 = np.zeros(192)
-kpoint11 = np.zeros(192)
-kpoint12 = np.zeros(192)
-kpoint13 = np.zeros(192)
-kpoint14 = np.zeros(192)
-kpoint15 = np.zeros(192)
-kpoint16 = np.zeros(192)
-kpoint17 = np.zeros(192)
-kpoint18 = np.zeros(192)
-kpoint19 = np.zeros(192)
-kpoint20 = np.zeros(192)
-kpoints = [kpoint1, kpoint2, kpoint3, kpoint4, kpoint5, kpoint6, kpoint7, kpoint8, kpoint9, kpoint10,
-           kpoint11, kpoint12, kpoint13, kpoint14, kpoint15, kpoint16, kpoint17, kpoint18, kpoint19, kpoint20]
-
-path = 'step2-test/OUTCAR'
-
-OUTCAR_file = open(path, "r")
-for x in range(1767):
-    OUTCAR_file.readline()
-
-for kpoint in kpoints:
-    for x in range(192):
-        actual_line = OUTCAR_file.readline()
-        energy_value = actual_line.split()[1]
-
-        kpoint[x] = energy_value
-
-    for x in range(3):
-        OUTCAR_file.readline()
-
-OUTCAR_file.close()
-
-band_matrix = np.zeros((192, 20))
-y = 0
-
-points_ordered = [1, 2, 3, 4, 7, 9, 10, 8, 5, 1, 11, 17, 20, 18, 13, 4, 20, 19, 16, 10]
-
-for kpoint in points_ordered:
-    for x in range(192):
-        band_matrix[x,y] = kpoints[kpoint-1][x]
-    y = y + 1
-
-
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
-kspace1 = np.linspace(0, 1, 16)
-kspace2 = np.linspace(1, 1.2, 4)
 
-interv1 = np.linspace(0, 1, 80)
-interv2 = np.linspace(1, 1.2, 16)
-k_space_interp = np.linspace(0, 1.2, 96)
+####################################################### IMPORTANT #######################################################
+# give the path to the OUTCAR file
+path_results = 'non-hybrid-bands/step2/OUTCAR' # path to OUTCAR
 
-band_matrix_interp = np.zeros((192, 96))
+# define here the k-path looking at the OUTCAR or KPOITNS files, the number in the k_points_in_path correspond 
+# to their position in the files, starting from 0. Then give a name to these points.
+k_points_in_path = [[0, 1, 2, 3, 4, 5, 6], [6, 15, 18], [18, 16, 12, 7, 0]] # index of k-points of interest
+sym_points_in_path = [0, 6, 18, 0] # index of high symmetry points in the path
+names_sym_points = ['$\\Gamma$', 'M', 'K', '$\\Gamma$'] # names of the high symmetry points
 
-for x in range(192):
-    band_matrix_interp[x,0:80] = interp1d(kspace1, band_matrix[x,0:16],
-                                  kind='quadratic', fill_value='extrapolate')(interv1)
-    band_matrix_interp[x,80:96] = interp1d(kspace2, band_matrix[x,16:20],
-                                  kind='quadratic', fill_value='extrapolate')(interv2)
+# define the density of your k_point mesh
+mesh_density = 1000 # number of points in the interpolated k-path
 
-plt.figure()
-plt.xlim(0, 1.2)
-plt.ylim(-7.5, 11.5)
-plt.ylabel('Energy (eV)')
+# set the energy limit values to represent in the plot
+min_energy = -5 # min value of the energy band to represent in the plot
+max_energy = 10 # max value of the energy band to represent in the plot
+#########################################################################################################################
 
-x_labels = ['$\\Gamma$', 'X', 'M', '$\\Gamma$', 'R', 'X|R', 'M']
-x_ticks = [0, 0.2, 0.4, 0.6, 0.8, 1, 1.2]
-for x in range(5):
-    plt.axvline(x_ticks[x], color='black', linestyle='--', linewidth=.8)
-plt.xticks(ticks=x_ticks, labels=x_labels)
 
-for x in range(192):
-    if x <= 22:
-        color_type = 'coral'
+# functions
+def get_bands(OUTCAR_path):
+    """
+    Get the energy bands from the OUTCAR file from a VASP calculation
+
+    Inputs:
+        OUTCAR_path -> path to the OUTCAR file
+    Outputs:
+        fermi_energy -> value of the fermi energy
+        num_kpoints -> number of k-points in the calculation
+        num_bands -> total number of energy bands
+        bands -> matrix with the energy bands with dimension num_kpoints x num_bands
+        k_points -> an array with the position of each k-point in the calculation
+    """
+
+    with open(OUTCAR_path, 'r') as file:
+        searc_pattern = 'E-fermi'
+
+        for line in file:
+            if searc_pattern in line:
+                fermi_energy = float(line.split()[2])
+
+    with open(OUTCAR_path, 'r') as file:
+        searc_pattern = 'NKPTS'
+
+        for line in file:
+            if searc_pattern in line:
+                num_kpoints = int(line.split()[3])
+                num_bands = int(line.split()[14])
+
+    total_num_lines = 0
+    finish_count = False
+    with open(OUTCAR_path, 'r') as file:
+        searc_pattern = 'E-fermi'
+
+        for line in file:
+            if finish_count == False:
+                total_num_lines = total_num_lines + 1
+            if searc_pattern in line:
+                finish_count = True
+
+    bands = np.zeros([num_kpoints, num_bands])
+    k_points = []
+
+    OUTCAR = open(OUTCAR_path, 'r')
+
+    for x in range(total_num_lines + 3):
+        line = OUTCAR.readline()
+
+    for kpoint in range(num_kpoints):
+        line = OUTCAR.readline()
+
+        k_pos = np.array([float(line.split()[3]), float(line.split()[4]), float(line.split()[5])])
+        k_points.append(k_pos)
+
+        line = OUTCAR.readline()
+
+        for band in range(num_bands):
+            line = OUTCAR.readline()
+
+            bands[kpoint, band] = float(line.split()[1])
+        
+        line = OUTCAR.readline()
+
+    OUTCAR.close()
+
+    return fermi_energy, num_kpoints, num_bands, bands, k_points
+
+def bands_segment(electronic_bands, segments, n_band):
+    """
+    It gives the values of the bands for each segment in the k-path
+
+    Inputs:
+        electronic_bands -> values of the band for all the path
+        segments -> array with different arrays representing each segment in the k-path
+        n_bands -> number of the electronic band
+    Outputs:
+        bands -> array with arrays of the energy bands for the different segments in the k-path
+        band_points -> an array with all the energy in k-path (without duplicate values)
+    """
+
+    bands = []
+
+    for segment in segments:
+        band = []
+        for k_point in segment:
+            band.append(electronic_bands[k_point, n_band])
+
+        bands.append(band)
+
+    it_seg = 0
+    band_points = []
+    for segment in bands:
+        if it_seg == 0:
+            for elem in segment:
+                band_points.append(elem)
+        else:
+            it_el = 0
+            for elem in segment:
+                if it_el != 0:
+                    band_points.append(elem)
+                it_el = it_el + 1
+        it_seg = it_seg + 1
+
+    return bands, band_points
+
+def find_minCB_maxVB(fermi_energy, bands):
+    """
+    Finds the minimum of the valence band and the maximum of the conduction band (for semiconductors and insulators)
+
+    Inputs:
+        fermi_energy -> energy value of the fermi level
+        bands -> values for all the energy bands
+    Outputs:
+        minCB -> minimum of the conduction band
+        maxVB -> maximum of the valence band
+        bandgap -> band gap of the material
+        type_of_gap -> if the material is a direct or indirect semiconductor
+    """
+
+    minCB = 1e5
+    maxVB = -1e5
+
+    k_VB = 0
+    k_CB = 0
+
+    for band in bands:
+        it_el = 0
+        for element in band:
+            if ((element - fermi_energy) < (minCB - fermi_energy)) and ((element - fermi_energy) > 0):
+                minCB = element
+                k_CB = it_el
+            if ((fermi_energy - element) < (fermi_energy - maxVB)) and ((fermi_energy - element) > 0):
+                maxVB = element
+                k_VB = it_el
+
+            it_el = it_el + 1
+
+    bandgap = minCB - maxVB
+
+    if k_CB == k_VB:
+        type_of_gap = 'Direct'
     else:
-        color_type = 'slateblue'
-    plt.plot(interv1, band_matrix_interp[x,0:80]-np.max(band_matrix[0:22,:]), color=color_type)
-    plt.plot(interv2, band_matrix_interp[x,80:96]-np.max(band_matrix[0:22,:]), color=color_type)
+        type_of_gap = 'Indirect'
 
-for x in range(22):
-    for y in range(16):
-        if band_matrix[x,y] == np.max(band_matrix[0:22,:]):
-            ind_max = y
-plt.plot(kspace1[ind_max], np.max(band_matrix[0:22,:])-np.max(band_matrix[0:22,:]), 'o', markersize=10, color='darkred')
-
-for x in range(23,192):
-    for y in range(16):
-        if band_matrix[x,y] == np.min(band_matrix[23:-1,:]):
-            ind_min = y
-plt.plot(kspace1[ind_min], np.min(band_matrix[23:-1,:])-np.max(band_matrix[0:22,:]), 'o', markersize=10, color='navy')
+    return minCB, maxVB, bandgap, type_of_gap
 
 
-plt.axvline(1, color='black', linewidth=1.2)
+# get the values
+fermi_energy, n_kpoints, n_bands, bands, k_points = get_bands(path_results)
 
-plt.savefig('energy-band.pdf')
+# interpolate the bands
+distance_between_sym_points = []
+for point in range(len(sym_points_in_path) - 1):
+    distance = np.linalg.norm(k_points[sym_points_in_path[point + 1]] - k_points[sym_points_in_path[point]])
+    distance_between_sym_points.append(distance)
 
-print('The maximum value is: ', np.max(band_matrix[0:22,:]))
-print('The minimum value is: ', np.min(band_matrix[23:-1,:]))
-print('Thus, the band gap is: ', np.min(band_matrix[23:-1,:]) - np.max(band_matrix[0:22,:]))
+points_to_interpol = []
+total_num_k_interp = 0
+for element in distance_between_sym_points:
+    points_to_interpol.append(int(element * mesh_density))
+    
+    total_num_k_interp = total_num_k_interp + int(element * mesh_density)
+
+final_k_path = np.linspace(0, 1, total_num_k_interp)
+
+k_new_interval_norm = []
+num_points = 0
+initial = 0
+for segment in points_to_interpol:
+    num_points = num_points + segment
+    final = num_points / total_num_k_interp
+
+    k_new_interval_norm.append(np.linspace(initial, final, segment))
+
+    initial = final
+
+total_distance = sum(distance_between_sym_points)
+k_old_interval_norm = []
+initial = 0
+distance = 0
+it_loop = 0
+for element in distance_between_sym_points:
+    distance = distance + element
+    final = distance / total_distance
+
+    k_old_interval_norm.append(np.linspace(initial, final, len(k_points_in_path[it_loop])))
+
+    initial = final
+    it_loop = it_loop + 1
+
+total_num_k_interp = total_num_k_interp - len(points_to_interpol) + 1
+k_points_paths_interp = []
+
+band_matrix_interp = []
+
+total_old_segment = []
+it_segment = 0
+for segment in k_old_interval_norm:
+    if it_segment == 0:
+        for element in segment:
+            total_old_segment.append(element)
+    else:
+        it_element = 0
+        for element in segment:
+            if it_element != 0:
+                total_old_segment.append(element)
+
+            it_element = it_element + 1
+    it_segment = it_segment + 1
+
+final_k_interval = np.linspace(0, 1, mesh_density)
+for band in range(n_bands):
+    _, band_segment = bands_segment(bands, k_points_in_path, band)
+
+    interpolated_band = interp1d(total_old_segment, band_segment, kind='quadratic', fill_value='extrapolate')(final_k_interval)
+
+    band_matrix_interp.append(interpolated_band)
+        
+
+minCB, maxVB, bg, type_gap = find_minCB_maxVB(fermi_energy, band_matrix_interp)
+print('################### ELECTRONIC RESULTS ###################')
+print(f'           The value of the band gap is:  {bg:.3f} eV')
+print(f'        The gap of the semiconductor is:  {type_gap}')
+print(f'              The fermi level energy is: {fermi_energy:.3f} eV')
+print(f'  The minimum of the conduction band is: {minCB:.3f} eV')
+print(f'     The maximum of the valence band is: {maxVB:.3f} eV')
+print('##########################################################')
+
+# adjust the bands to maxVB=0
+band_matrix_interp_shift = []
+for band in band_matrix_interp:
+    new_band = []
+    for element in band:
+        new_band.append(element - maxVB)
+    band_matrix_interp_shift.append(new_band)
+        
+
+# plot the bands for the desired path
+plt.figure()
+fig, axs = plt.subplots(figsize=(5, 4))
+
+it_segment = 0
+for segment in k_old_interval_norm:
+    if it_segment != 0:
+        axs.axvline(segment[0], color='black', linestyle='--', linewidth=0.8)
+    
+    it_segment = it_segment + 1
+
+axs.set_xlim(0, 1)
+axs.set_ylim(min_energy, max_energy)
+
+axs.set_ylabel('Energy (eV)')
+
+for band in range(n_bands):
+    axs.plot(final_k_interval, band_matrix_interp_shift[band], color='black')
+
+new_labels = names_sym_points
+old_labels = []
+it_segment = 0
+for segment in k_new_interval_norm:
+    if it_segment == 0:
+        old_labels.append(segment[0])
+
+    old_labels.append(segment[-1])
+
+    it_segment = it_segment + 1
+axs.set_xticks(ticks=old_labels, labels=new_labels)
+
+plt.tight_layout()
+plt.savefig('bands.pdf')
