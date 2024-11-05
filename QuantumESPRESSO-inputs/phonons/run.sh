256d73e5c3479521ad6f98f7b09a0508e4360c27
Pol <polbeco@gmail.com>
2024-11-05 00:09:13 +0000

    phonon calculation with QuantumESPRESSO input files and bash file

--- QuantumESPRESSO-inputs/phonons/run.sh
+++ QuantumESPRESSO-inputs/phonons/run.sh
@@ -8,7 +8,9 @@
 
 module load fftw/3.3.10
 module load hdf5/1.14.1-2
-module load quantumespresso/7.2
+module load quantumespresso/6.5
 
 time srun   pw.x -input scf.in > scf.out
 time srun   ph.x -input ph.in > ph.out
+time srun   q2r.x -input q2r.in > q2r.out
+time srun   matdyn.x -input matdyn.in > matdyn.out
